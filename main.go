199b555e5c4f03796a94f56f9de05e8d7958a866
msrocka <michael.srocka@gmail.com>
2018-03-16 14:25:37 +0100

    remove the calculation experiment

--- main.go
+++ main.go
@@ -12,8 +12,6 @@ func main() {
 		NewFlowMapper(args).Run()
 	case "merge":
 		NewMerger(args).Run()
-	case "calc":
-		NewCalculator(args).Run()
 	default:
 		log.Fatalln("ERROR: Unknown command", args.Command)
 	}
